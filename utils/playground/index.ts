ae1dfc08db6af6d8fa0e6baeef66bd5043ca0e98
Stella <hcox.dev@gmail.com>
2024-08-21 01:45:23 -0400

    refactor(Playground): update GitHub link in playground index.ts

--- utils/playground/index.ts
+++ utils/playground/index.ts
@@ -6,6 +6,6 @@ export const PlaygroundItems = [
 			"Simulations are a great way to learn and understand complex systems. Here are some simulations that I've created.",
 		module: import('./code/simulations'),
 		github:
-			'https://github.com/stellaOnEstrogen/website/blob/main/src/utils/playground/code/simulations.ts',
+			'https://github.com/stellaOnEstrogen/website/blob/main/utils/playground/code/simulations.ts',
 	},
 ];
